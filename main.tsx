493ff6d8f518bf905c2911839840771b47abe683
Max Musial <144265654+masterpillow@users.noreply.github.com>
2025-09-26 17:56:07 -0400

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Homepage from "./src/pages/Homepage";
+import Homepage from "./pages/Homepage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
