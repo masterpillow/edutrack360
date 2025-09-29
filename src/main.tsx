b438d973467f0ac88bf1ad43f9c0a7b215bf80ea
Max Musial <144265654+masterpillow@users.noreply.github.com>
2025-09-29 22:03:27 +0000

    feat: add landing page with background and working React setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Homepage from "./pages/Homepage";
+import "./styles/landing.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
